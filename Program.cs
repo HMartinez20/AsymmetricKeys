3dc88f38360b5403dd3595de2b39dc989e295354
Hailey Martinez <hmartinez21@mail.stmarytx.edu>
2020-02-24 23:27:40 -0600

    Finished[?] code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -11,86 +12,108 @@ namespace HashExample
     {
         static void Main(string[] args)
         {
-             /** INSTRUCTIONS:
-             * Implement a program in C# to sign a document. The program 
-             * will be able to generate a public key pair, accept a document 
-             * and compute the hash.  It then encrypts the has using the 
-             * private key.  The program will be able to verify a signature 
-             * by accepting an encrypted signature (hash) and a document.  
-             * It computes the hash, decrypts the encrypted hash and compares 
-             * the hashes. It reports if the document is valid or not.  Note, 
-             * C# provides the necessary cryptographic primitives.  Use ONLY 
-             * the hash and public key encryption functions, i.e. write your 
-             * own digital signature functions. 
+            /** INSTRUCTIONS:
+            * Implement a program in C# to sign a document. The program 
+            * will be able to generate a public key pair, accept a document 
+            * and compute the hash.  It then encrypts the has using the 
+            * private key.  The program will be able to verify a signature 
+            * by accepting an encrypted signature (hash) and a document.  
+            * It computes the hash, decrypts the encrypted hash and compares 
+            * the hashes. It reports if the document is valid or not.  Note, 
+            * C# provides the necessary cryptographic primitives.  Use ONLY 
+            * the hash and public key encryption functions, i.e. write your 
+            * own digital signature functions. 
+            **/
+
+            /* REFERENCE: https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.rsacryptoserviceprovider.signhash?view=netframework-4.8 */
+
+            /** STEPS
+             * Sender:
+             * 1. Generate keys
+             * 2. Get document
+             * 3. Encrypt document
+             * 4. Get hash
+             * 5. Encrypt hash for signature
+             *    
+             * Receiver:
+             * 1. Verify signature (encrypted hash) & document
+             *    a. Generate hash
+             *    b. Verify document and signature
+             *    c. Decrypt document
              **/
 
-            try
-            {
-                // Create keys for Alice and Bob, then save them in a containers.
-                GenKey_SaveInContainer("Alice");
-                GenKey_SaveInContainer("Bob");
+            /* STEP ONE -  Gen Keys */
+            // May also use CspParameters.KeyContainerName to store keys in safe containers
+            RSACryptoServiceProvider AliceKey = new RSACryptoServiceProvider();
+            RSAParameters AlicePublic = AliceKey.ExportParameters(false);
+            RSAParameters AlicePrivate = AliceKey.ExportParameters(true);
 
-                // Retrieve the keys from the containers.
-                GetKeyFromContainer("Alice");
-                GetKeyFromContainer("Bob");
-
-                // Delete the keys from the containers.  
-                DeleteKeyFromContainer("Alice");
-                DeleteKeyFromContainer("Bob");
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            RSACryptoServiceProvider BobKey = new RSACryptoServiceProvider();
+            RSAParameters BobPublic = BobKey.ExportParameters(false);
+            RSAParameters BobPrivate = BobKey.ExportParameters(true);
 
 
-            static void GenKey_SaveInContainer(string ContainerName)
-            {
-                // Create container to hold key. 
-                CspParameters cp = new CspParameters();
-                cp.KeyContainerName = ContainerName;
+            /* STEP TWO - Get Doc*/
+            string path = @"C:\Users\Jammer\source\repos\HashExample\privateDoc.txt";
+            byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+            FileStream readFS = File.Open(path, FileMode.Open);
+            readFS.Read(fileBytes, 0, System.Convert.ToInt32(readFS.Length));
+            readFS.Close();
+            FileStream fs = File.Open(path, FileMode.Open);
 
-                // Create instance of RSACryptoServiceProvider to access 
-                // the key container.
-                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
+            /* STEP THREE - Encrypt Doc */
+            BobKey.ImportParameters(BobPublic);
+            byte[] encDoc = BobKey.Encrypt(fileBytes, false);
+            Console.WriteLine("Encrypted Doc using Bob's public key");
+            for (int i = 0; i < encDoc.Length; i++) { Console.Write($"{encDoc[i]:X2}"); }
+            Console.WriteLine("\n");
+            
+            
+            /* STEP FOUR - Compute Hash With Doc */
+            fs.Position = 0;
+            SHA256 sha256Hash = SHA256.Create();
+            byte[] hash = sha256Hash.ComputeHash(encDoc);
+            Console.Write("Original Doc Hash: ");
+            for (int i = 0; i < hash.Length; i++) { Console.Write($"{hash[i]:X2}"); }
+            Console.WriteLine("\n");
 
-                Console.WriteLine("Key added to container: \n  {0}", rsa.ToXmlString(true));
-            }
 
-            static void GetKeyFromContainer(string ContainerName)
-            {
-                // Similar to GenKey, BUT if container doesn't exist,
-                // the key is generated and stored into the new container.
-                // If the container exists, the key is loaded from the
-                // container.
-                CspParameters cp = new CspParameters();
-                cp.KeyContainerName = ContainerName; 
-                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
-                Console.WriteLine("Key retrieved from container : \n {0}", rsa.ToXmlString(true));
+            /* STEP FIVE - Encrypt Hash */
+            AliceKey.ImportParameters(AlicePrivate);
+            byte[] signature = AliceKey.SignHash(hash, CryptoConfig.MapNameToOID("SHA256"));
+            Console.WriteLine("Encrypted Hash using Alice's private key");
+            for (int i = 0; i < signature.Length; i++) { Console.Write($"{signature[i]:X2}"); }
+            Console.WriteLine("\n");
 
-                /* The key's Modulus is the public key */
-            }
 
-            static void DeleteKeyFromContainer(string ContainerName)
-            {
-                // Create the CspParameters object and set the key container   
-                // name used to store the RSA key pair.  
-                CspParameters cp = new CspParameters();
-                cp.KeyContainerName = ContainerName;
+            /* VERIFY SIGNATURE */
+            /* STEP ONE, a - Generate New Hash */
+            fs.Position = 0;
+            SHA256 newSha256Hash = SHA256.Create();
+            byte[] newHash = newSha256Hash.ComputeHash(encDoc);
+            Console.Write("Re-Hash of Original: ");
+            for (int i = 0; i < newHash.Length; i++) { Console.Write($"{newHash[i]:X2}"); }
+            Console.WriteLine("\n");
 
-                // Create a new instance of RSACryptoServiceProvider that accesses  
-                // the key container.  
-                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
 
-                // Delete the key entry in the container.  
-                rsa.PersistKeyInCsp = false;
+            /* STEP ONE, b - Verification */
+            AliceKey.ImportParameters(AlicePublic);
+            bool dataVerified = AliceKey.VerifyData(encDoc, CryptoConfig.MapNameToOID("SHA256"), signature);
+            Console.WriteLine("Verified document? {0}\n", dataVerified? "True" : "False");
 
-                // Call Clear to release resources and delete the key from the container.  
-                rsa.Clear();
+            bool signatureVerified = AliceKey.VerifyHash(newHash, CryptoConfig.MapNameToOID("SHA256"), signature);
+            Console.WriteLine("Verified signature? {0}\n", signatureVerified ? "True" : "False");
 
-                Console.WriteLine("Key deleted.");
-            }
+            /* STEP ONE, c - Decrypt Doc*/
+            BobKey.ImportParameters(BobPrivate);
+            byte[] decDoc = BobKey.Decrypt(encDoc, false);
+            Console.WriteLine("Decrypted document: \n{0}\n", new ASCIIEncoding().GetString(decDoc));
 
+            fs.Close();
+
+            /* REMOVE KEYS */
+            AliceKey.Clear();
+            BobKey.Clear();
         }
     }
 }
